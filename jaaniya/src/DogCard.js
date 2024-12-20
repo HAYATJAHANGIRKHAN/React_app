5214de34ded2ae73a68b1dfbf4d2a273194aed7a
Hayat <hayatjahangir0@gmail.com>
2024-12-21 00:07:14 +0530

    create dog card and add images

---
+++ jaaniya/src/DogCard.js
@@ -0,0 +1,12 @@
+function DogCard () {
+    return (
+        <div>
+        <h1>Dog Card</h1>
+        <p>This is a dog card.</p>
+        <img className="dog" src="https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg" />
+        
+        </div>
+    );
+};
+
+export default DogCard;
