ac7ce2abc8c1b04786c2dc08c7f8bd30ab3ef0dd
Hayat <hayatjahangir0@gmail.com>
2024-12-23 20:29:55 +0530

    add some coditions

--- jaaniya/src/counter.js
+++ jaaniya/src/counter.js
@@ -1,15 +1,42 @@
+import { useState } from "react";
 
-import {useState}from 'react';
+// import {useState}from 'react';
+// function Counter(){
+//  const [x,setx]=useState(0);
+
+// return(
+//     <>
+//     count:{x}
+//     <button onClick={()=>setx(x+1)}>Increament</button>
+//     <button onClick={() => setx(x-1)}>decreament</button>
+//     </>
+// )
+// }
+
+// export default Counter;
+
 function Counter(){
- const [x,setx]=useState(0);
+    const[x,setx]=useState(0);
+    if(x==10){
+        alert("reached 10");
+        setx(0);
+        return null;  // to avoid rendering the rest of the component
+    } else if(x==-1){
+        alert("reached -1");
+        setx(0);
+        return null;  // to avoid rendering the rest of the component
+    }
+    return(
+        <>
+        count{x}
+        <button onClick={()=>setx(x+1)}>inc</button>
+        <button onClick={()=>setx(x-1)}>dec</button>
+        <button onClick={()=>setx(0)}>reset</button>
+       
 
-return(
-    <>
-    count:{x}
-    <button onClick={()=>setx(x+1)}>Increament</button>
-    <button onClick={() => setx(x-1)}>decreament</button>
-    </>
-)
+        
+        </>
+    )
 }
 
 export default Counter;
