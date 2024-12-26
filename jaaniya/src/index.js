d55bc197b1058a11cbefc02c48d84bc8eff81ab5
Hayat <hayatjahangir0@gmail.com>
2024-12-26 20:22:35 +0530

    add redux

--- jaaniya/src/index.js
+++ jaaniya/src/index.js
@@ -7,7 +7,6 @@ import Card from './components/Card/Card';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>HAYAT JAHNGIR KHAN</h1>
-    <Card player="O" />
+    
   </React.StrictMode>
 );
